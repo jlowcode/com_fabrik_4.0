9a0c6b03233ed431e95b0f5c0ff4e66ffadcc3d3
Diego-Godina <diegosantos.2001@alunos.utfpr.edu.br>
2024-10-09 16:21:41 -0300

    Line heigth

--- site/views/details/tmpl/jlowcode_admin/custom_css.php
+++ site/views/details/tmpl/jlowcode_admin/custom_css.php
@@ -38,6 +38,10 @@ $c = $_REQUEST['c'];
 echo <<<EOT
 /* BEGIN - Your CSS styling starts here */
 
+.fabrikElementReadOnly {
+    line-height: 20px;
+}
+
 .modal-content {
     border-radius: 20px !important;
     box-shadow: 0px 6px 6px 0px #3c3939;
