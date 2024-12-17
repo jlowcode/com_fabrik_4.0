1ffb35e3ed3c309c0a99763bf83cc376d5c61914
Diego-Godina <diegosantos.2001@alunos.utfpr.edu.br>
2024-12-17 11:20:09 -0300

    Icon arrow

--- site/views/list/tmpl/jlowcode_admin/default_row_tree.php
+++ site/views/list/tmpl/jlowcode_admin/default_row_tree.php
@@ -12,30 +12,29 @@
 
 // No direct access
 defined('_JEXEC') or die('Restricted access');
-
 ?>
-
 
 <div class="tree-item" data-id=<?php echo @$this->_row->{$this->list->db_table_name.'___id_raw'}; ?>>
-    <span class="tree-arrow" onclick="carregarFilhos(<?php echo @$this->_row->{$this->list->db_table_name.'___id_raw'}; ?>, this.parentElement)">▶</span>
-    <span class="tree-text"> <?php echo @$this->_row->{$this->list->db_table_name.'___name_raw'}; ?></span>
+    <span class="tree-arrow" onclick="carregarFilhos(<?php echo @$this->_row->{$this->list->db_table_name.'___id_raw'}; ?>, this.parentElement)">
+		<?php echo FabrikHelperHTML::image('arrow-round-right.png', 'list', $this->tmpl);?>
+	</span>
 
-<?php foreach ($this->headings as $heading => $label) :
-	if ($heading == 'fabrik_actions') : 
-	// $d = @$this->_row->data->$heading;
-	$d = @$this->_row->$heading;
+	<span class="tree-text">
+		<?php echo @$this->_row->{$this->list->db_table_name.'___name_raw'}; ?>
+	</span>
+
+	<?php 
+		foreach ($this->headings as $heading => $label) :
+			if ($heading == 'fabrik_actions') :
+				$d = @$this->_row->$heading;
+				$d = str_replace('class="delete"','class="btn-delete"  data-rowid="xhr" data-loadmethod="xhr" target="_self" list-row-ids="'.$this->list->id.':'.$this->_row->__pk_val.'" ',$d);
+				$d = str_replace('Excluir','Reportar/Excluir',$d);
+				$d = str_replace('href="#"','onclick="onReportAbuse(this)"',$d);
+
+				echo '<span class="actions' . $c['class'] . '" ' . $cStyle . '>' . $d . '</span>'; ?>
+			<?php
+			endif;
+		endforeach;
 	?>
-	
-		<?php 
-		$d = str_replace('class="delete"','class="btn-delete"  data-rowid="xhr" data-loadmethod="xhr" target="_self" list-row-ids="'.$this->list->id.':'.$this->_row->__pk_val.'" ',$d);
-		$d = str_replace('Excluir','Reportar/Excluir',$d);
-		$d = str_replace('href="#"','onclick="onReportAbuse(this)"',$d);
-		//$d = str_replace('href="#"','',$d);
-
-		echo '<span class="actions' . $c['class'] . '" ' . $cStyle . '>' . $d . '</span>'; ?>
-	<?php
-	endif;
-endforeach;
-?>
 </div>
 
