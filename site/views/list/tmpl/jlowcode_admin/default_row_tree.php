1f38d4e815cf15e94ec35504e99310bebac1583e
Diego-Godina <diegosantos.2001@alunos.utfpr.edu.br>
2025-04-09 15:08:06 -0300

    feat: diferent labels to report/delete records

--- site/views/list/tmpl/jlowcode_admin/default_row_tree.php
+++ site/views/list/tmpl/jlowcode_admin/default_row_tree.php
@@ -3,7 +3,7 @@
  * Fabrik List Template: Div Row
  * Note the div cell container is now generated in the default template
  * in FabrikHelperHTML::bootstrapGrid();
- *
+ * 
  * @package     Joomla
  * @subpackage  Fabrik
  * @copyright   Copyright (C) 2005-2016  Media A-Team, Inc. - All rights reserved.
@@ -12,6 +12,16 @@
 
 // No direct access
 defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\Language\Text;
+
+$pluginManager = FabrikWorker::getPluginManager();
+$elementModel  = $pluginManager->getPlugIn('workflow', 'form');
+$elementModel->easyadmin = true;
+$hasPermission = $elementModel->hasPermission(['easyadmin_modal___listid' => $this->getModel()->getId()]);
+$titleDelAction = $hasPermission ? Text::_("PLG_FORM_WORKFLOW_DELETE_RECORD_LIST") : Text::_("PLG_FORM_WORKFLOW_REPORT_RECORD_LIST");
+$imgDelAction = $hasPermission ? 'trash.png' : 'danger.png';
+
 ?>
 
 <div class="tree-item" data-id=<?php echo @$this->_row->{$this->list->db_table_name.'___id_raw'}; ?>>
@@ -23,13 +33,14 @@ defined('_JEXEC') or die('Restricted access');
 		<?php echo @$this->_row->{$this->list->db_table_name.'___name_raw'}; ?>
 	</span>
 
-	<?php 
+	<?php
 		foreach ($this->headings as $heading => $label) :
 			if ($heading == 'fabrik_actions') :
 				$d = @$this->_row->$heading;
-				$d = str_replace('class="delete"','class="btn-delete"  data-rowid="xhr" data-loadmethod="xhr" target="_self" list-row-ids="'.$this->list->id.':'.$this->_row->__pk_val.'" ',$d);
-				$d = str_replace('Excluir','Reportar/Excluir',$d);
-				$d = str_replace('href="#"','onclick="onReportAbuse(this)"',$d);
+				$d = str_replace('class="delete"','class="btn-delete"  data-rowid="xhr" data-loadmethod="xhr" target="_self" list-row-ids="' .$this->list->id . ':' . $this->_row->__pk_val . '" ', $d);
+				$d = str_replace('Excluir', $titleDelAction, $d);
+				$d = str_replace('close.png', $imgDelAction, $d);
+				$d = str_replace('href="#"','onclick="onReportAbuse(this)"', $d);
 
 				echo '<span class="actions' . $c['class'] . '" ' . $cStyle . '>' . $d . '</span>'; ?>
 			<?php
