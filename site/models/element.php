40ea4851446b4d4455e1f731d1b058d499104a8a
Diego-Godina <diegosantos.2001@alunos.utfpr.edu.br>
2025-05-23 06:30:56 -0300

    fix: filter with all option on stars is not needed

--- site/models/element.php
+++ site/models/element.php
@@ -8582,7 +8582,7 @@ class PlgFabrik_Element extends FabrikPlugin
 		$icon = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-star-fill' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
   					<path d='M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z'/>
 				</svg>&nbsp;";
-		$labels[] = 'All';
+		//$labels[] = 'All'; // All option is not needed
 
 		for ($i=5; $i > 0; $i--) { 
 			$label = "";
@@ -8592,7 +8592,8 @@ class PlgFabrik_Element extends FabrikPlugin
 			$labels[] = $label;
 		}
 
-		$values = array('', '5.00', '4.00', '3.00', '2.00', '1.00'); // static values of the 'rating' element
+		//$values = array('', '5.00', '4.00', '3.00', '2.00', '1.00'); // static values of the 'rating' element
+		$values = array('5.00', '4.00', '3.00', '2.00', '1.00'); // static values of the 'rating' element
 		
 		$params = $this->getParams();
 
