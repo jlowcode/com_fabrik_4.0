6d611d41fa64d64702ecb099eadb1104d8ade863
Diego-Godina <diegosantos.2001@alunos.utfpr.edu.br>
2025-07-12 17:18:33 -0300

    fix: validation failed not clean the filled fields anymore

--- site/models/element.php
+++ site/models/element.php
@@ -3752,7 +3752,7 @@ class PlgFabrik_Element extends FabrikPlugin
 		if ($pop !== '')
 		{
 			$w    = new FabrikWorker;
-			$data = empty($data) ? $this->getFormModel()->getData() : $data;
+			$data = empty($data) ? $this->getFormModel()->data : $data;
 			$pop  = $w->parseMessageForPlaceHolder($pop, $data, false);
 
 			$key = md5($pop) . '-' . md5(serialize($data));
