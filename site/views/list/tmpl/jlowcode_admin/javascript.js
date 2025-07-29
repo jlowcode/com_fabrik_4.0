6875872ed1f13b593eb4d9c46780e1eff3dc4ed0
Diego-Godina <diegosantos.2001@alunos.utfpr.edu.br>
2025-07-29 10:51:03 -0300

    chore: correct indentantion

--- site/views/list/tmpl/jlowcode_admin/javascript.js
+++ site/views/list/tmpl/jlowcode_admin/javascript.js
@@ -8,11 +8,11 @@
 requirejs(['fab/fabrik', 'fab/bootstrap_tree'], function (Fabrik, BootstrapTree) {
 	var tree = jQuery('.summary')[0];
 
-  setFiltersTutorialTemplate();
-  orderingTreeTutorial(tree);
-  setToogleFilters();
-  setNumFlagsAndClearFilters();
-  checkViewMobileMode();
+	setFiltersTutorialTemplate();
+	orderingTreeTutorial(tree);
+	setToogleFilters();
+	setNumFlagsAndClearFilters();
+	checkViewMobileMode();
 
 	Fabrik.addEvent('fabrik.list.updaterows', function () {
 		jQuery('.subRenderCard .description').each(function () {
