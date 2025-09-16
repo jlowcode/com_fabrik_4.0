099b4aba6bb2e8ca981b49fddadf8504091d1004
Diego-Godina <diegosantos.2001@alunos.utfpr.edu.br>
2025-09-16 08:01:36 -0300

    fix: learn-more for small descriptions

--- site/views/list/tmpl/jlowcode_admin/default_header.php
+++ site/views/list/tmpl/jlowcode_admin/default_header.php
@@ -99,9 +99,11 @@ if (isset($_REQUEST['action']) && isset($_REQUEST['action']['showButton'])) {
                         <div class="intro-container <?php echo $this->modalLearnMore['callModal'] ? 'd-none' : ''; ?>">
                             <div class="text-intro-content">
                                 <p><?php echo strip_tags($this->table->intro); ?></p>
-                                <a class="learn-more" href="#<?php echo $idModalLearnMore ?>" data-bs-toggle="modal">
-                                    <?php echo Text::_("JGLOBAL_LEARN_MORE") ?>
-                                </a>
+                                <?php if(strlen($this->table->intro) > 180) : ?>
+                                    <a class="learn-more" href="#<?php echo $idModalLearnMore ?>" data-bs-toggle="modal">
+                                        <?php echo Text::_("JGLOBAL_LEARN_MORE") ?>
+                                    </a>
+                                <?php endif; ?>
                             </div>
                         </div>
                     <?php endif; ?>
