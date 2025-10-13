a4806c6cd8486c97bf4594386ea05baa3a88bebc
Diego-Godina <diegosantos.2001@alunos.utfpr.edu.br>
2025-10-13 13:17:42 -0300

    fix: revert getValue to image element on mansory mode

--- site/views/list/tmpl/jlowcode_admin/default_row_masonry.php
+++ site/views/list/tmpl/jlowcode_admin/default_row_masonry.php
@@ -31,11 +31,8 @@ $elements = $this->getModel()->getElements('filtername');
 				<div class="fabrikDivElement">
 					<span class="thumb <?= $thumbFullName ?>" >
 						<?php
-							$thumbRawName = $thumbFullName . '_raw';
-							$allData = @$rowData ?? new stdClass();
-							$ignoreHeadings[] = $thumbFullName;
-
-							echo $el->renderListData(@$rowData->$thumbRawName, $allData);
+                            echo $el->getValue((array) @$rowData);
+                            $ignoreHeadings[] = $thumbFullName;
 						?>
 					</span>
 				</div>
