f8c85bbfb377d044269b8e7a97e3d5447c64d076
David-Lucas17 <lucasehermilda@gmail.com>
2025-10-16 16:18:53 -0300

    style: I adjusted the styling of the tags in the add record form

--- site/views/form/tmpl/jlowcode_admin/custom_css.php
+++ site/views/form/tmpl/jlowcode_admin/custom_css.php
@@ -383,7 +383,11 @@ main .platform-content {
 	color: #C2F6F9 !important;
 }
 
-
+.chosen-choices .search-choice {
+    background-color:#CDE6FB !important;
+	border: none !important;
+	background-image: none !important;
+}
 
 /* END - Your CSS styling ends here */
 
