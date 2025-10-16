c865b314ec967e4a77b258425fd8ab5b0798d780
David-Lucas17 <lucasehermilda@gmail.com>
2025-10-16 16:15:14 -0300

    feat: copy url button added in details

---
+++ site/views/details/tmpl/jlowcode_admin/javascript.js
@@ -0,0 +1,50 @@
+/**
+ * Details helper
+ *
+ * @copyright: Copyright (C) 2025  Media A-Team, Inc. - All rights reserved.
+ * @license  : GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+// details-share.js - código sem requirejs
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        const shareButton = document.querySelector('.btn-share');
+        const pageHeader = document.querySelector('.page-header');
+        const titleH1 = document.querySelector('.page-header h1');
+        
+        if (shareButton && pageHeader && titleH1) {
+            titleH1.parentNode.insertBefore(shareButton, titleH1.nextSibling);
+            pageHeader.style.display = 'flex';
+            pageHeader.style.alignItems = 'center';
+            pageHeader.style.gap = '24px';
+            titleH1.style.margin = '0';
+            
+            jQuery(shareButton).css({
+                'background-color': 'rgba(220, 226, 249, 1)',
+                'border-radius': '50%',
+                'width': '40px',
+                'height': '40px',
+                'padding': '0px',
+                'flex-shrink': '0'
+            }).find('img').css({
+                'margin-bottom': '3px'
+            });
+        }
+        
+        function setCopyToClipboard() {
+            jQuery(document).off('click', '.btn-share');
+            jQuery('.btn-share').on('click', function() {
+                var tempInput = document.createElement('input');
+                tempInput.value = window.location.href;
+                document.body.appendChild(tempInput);
+                tempInput.select();
+                tempInput.setSelectionRange(0, 99999);
+                document.execCommand('copy');
+                document.body.removeChild(tempInput);
+                alert('URL copiada para a área de transferência!');
+            });
+        }
+        
+        setCopyToClipboard();
+    }, 100);
+});
