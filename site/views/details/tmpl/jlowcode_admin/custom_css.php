f94f013d399354871ad9837fcf314319bd8209c9
Diego-Godina <diegosantos.2001@alunos.utfpr.edu.br>
2025-11-14 13:35:36 -0300

    refactor: remove unecessary changes

--- site/views/details/tmpl/jlowcode_admin/custom_css.php
+++ site/views/details/tmpl/jlowcode_admin/custom_css.php
@@ -38,16 +38,8 @@ $c = $_REQUEST['c'];
 echo <<<EOT
 /* BEGIN - Your CSS styling starts here */
 
-.plg-display .header {
-    margin-bottom: 0 !important;
+.plg-display .header-desc, .plg-display .header-thumb {
     display: none !important;
-}
-
-.plg-display .fabrikButtonsContainer .middle-buttons .itemShowRequests {
-    display: none !important;
-    list-style: none !important;
-    margin: 0 !important;
-    padding: 0 !important;
 }
 
 .fabrik_groupdata td, .list-footer {
